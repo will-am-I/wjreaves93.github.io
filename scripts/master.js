22057ea6336fd77d067b2eb53b93620c32850df4
Will Reaves <wjreaves93@yahoo.com>
2017-03-29 15:32:41 -0500

    created master setup

---
+++ scripts/master.js
@@ -0,0 +1,11 @@
+$(document).ready(function()
+{
+	$('#header').ready(function()
+	{
+		$('#header').load('pages/header.html');
+	});
+
+	$('#footer').ready(function(){
+		$('#footer').load('pages/footer.html');
+	});
+});
