cb19159399f8f86ebcbd4edb522f02948a7bf195
wjreaves93 <wjreaves93@yahoo.com>
2017-04-03 15:06:24 -0500

    hopefully fixed body loading issue

--- scripts/body.js
+++ scripts/body.js
@@ -1,32 +1,29 @@
 $(document).ready(function()
 {
-	$('#header').ready(function()
+	$('#body').ready(function()
 	{
-		$('#body').ready(function()
+		$('#about').click(function()
 		{
-			$('#about').click(function()
-			{
-				$('#body').empty();
-				$('#body').load('pages/about.html');
-			});
-			
-			$('#portfolio').click(function()
-			{
-				$('#body').empty();
-				$('#body').load('pages/portfolio.html');
-			});
-			
-			$('#resume').click(function()
-			{
-				$('#body').empty();
-				$('#body').load('pages/resume.html');
-			});
-			
-			$('#clients').click(function()
-			{
-				$('#body').empty();
-				$('#body').load('pages/clients.html');
-			});
+			$('#body').empty();
+			$('#body').load('pages/about.html');
+		});
+
+		$('#portfolio').click(function()
+		{
+			$('#body').empty();
+			$('#body').load('pages/portfolio.html');
+		});
+
+		$('#resume').click(function()
+		{
+			$('#body').empty();
+			$('#body').load('pages/resume.html');
+		});
+
+		$('#clients').click(function()
+		{
+			$('#body').empty();
+			$('#body').load('pages/clients.html');
 		});
 	});
 });
